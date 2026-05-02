cb4f911cfd55ac413eb3de36e6a24b648336e85a
Mukhammad Roykhan Abidin <dederoykhan@gmail.com>
2026-05-02 13:14:00 +0700

    feat: inisialisasi project Vite React

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="starter-page">
+      <h1>Sistem Manajemen Mahasiswa</h1>
+      <p>Project React Vite untuk UTS Pemrograman Web Berbasis Framework.</p>
+    </main>
+  );
+}
+
+export default App;
